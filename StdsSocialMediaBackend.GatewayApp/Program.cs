5d62c5adbaa1afce1297168222ef84f9503317a0
Marvin Seidel <marvin.seidel@klengel.de>
2023-05-30 21:47:37 +0200

    cors neu

--- StdsSocialMediaBackend.GatewayApp/Program.cs
+++ StdsSocialMediaBackend.GatewayApp/Program.cs
@@ -1,5 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
 using StdsSocialMediaBackend.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,11 +10,21 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", false, true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.WithOrigins("http://localhost")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
 builder.Services.RegisterInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
 await app.UseOcelot();
+
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
